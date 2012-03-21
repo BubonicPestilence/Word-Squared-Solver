b851fe3e6d0c2d4716dc21b189066e22a31c3e2f
Bubonic Pestilence <me@bubonicpestilence.ru>
2012-03-21 15:50:08 +0300

    TODO & Comments

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- Possible bug: locked in their own space, and will use all stars due to fails
+- Rewrite response handling to another class
--- settings.example.rb
+++ settings.example.rb
@@ -5,4 +5,5 @@
 $username = "PUT HERE YOUR LOGIN"
 $password = "PUT HERE YOUR PASSWORD"
 
+# Set this variable to amount of tries before using "swap tiles" features, or set it to false to not use "swap tiles"
 $fails_before_swap_tiles = 3
